f9d376697ef2e32e57ecd5a750da28c02c285965
magarsa <safalranamagar@gmail.com>
2020-02-16 16:33:23 -0600

    adding service respons

--- contacts-manager/src/app/contacts/contacts.component.ts
+++ contacts-manager/src/app/contacts/contacts.component.ts
@@ -1,3 +1,4 @@
+import { ContactsService } from './contacts.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit(): void {
+    this.contactsService.getAgents().subscribe(response => {
+      console.log(response);
+    })
   }
 
 }
--- contacts-manager/src/app/contacts/contacts.service.ts
+++ contacts-manager/src/app/contacts/contacts.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ServiceResponse } from '../models/serviceResponse';
-import { Agent } from 'http';
+import { Agent } from '../models/agent';
+import { ErrorResponse } from '../models/errorResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
   basePath: string;
+  mockAgentList: [];
+  response: ServiceResponse<Agent[]> = new ServiceResponse(
+    {
+      data: [
+        new Agent({
+          firstName: 'Safal',
+          lastName: 'Rana',
+          email:'safal-r@emcins.com',
+          phone:5156667777,
+          active: true
+        })
+      ],
+      error: new ErrorResponse({
+        isError: false,
+        errors: []
+      })
+    }
+    
+  )
 
   constructor(protected httpClient: HttpClient) { 
     this.basePath = 'http://localhost:4200/'
@@ -16,6 +36,7 @@ export class ContactsService {
 
   public getAgents(): Observable<ServiceResponse<Agent[]>>{
     const fullPath: string = this.basePath.concat('/api/agents');
-    return this.httpClient.get<ServiceResponse<Agent[]>>(fullPath);
+    // return this.httpClient.get<ServiceResponse<Agent[]>>(fullPath);
+    return of(this.response);
   }
 }
